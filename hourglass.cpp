fa4082878929e9514fb88f7bd96353624ae1c809
ananyakudtarkar <ananyask.03@gmail.com>
2025-07-24 10:29:49 +0530

    Create hourglass.cpp

---
+++ hourglass.cpp
@@ -0,0 +1,43 @@
+// Hourglass
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    
+    for (int i = n; i >= 1; --i) { 
+        for (int j = 1; j <= (n - i); ++j) {
+            cout << " ";
+        }
+        // Inner loop to print characters
+        for (int k = 0; k < i; ++k) {
+            cout << "* ";
+        }
+        cout<<endl;
+    }
+
+    for(int i = 2 ;i <=n ; i++){
+        for(int j = 1; j <= n - i ; j++){
+            cout<<" ";
+        }
+        for(int k = 1; k <= i; k++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+/*
+OUPUT:
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+
+*/
