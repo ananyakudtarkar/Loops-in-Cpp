8379aa123a7fe6d2e5481ba83b3fc7ce2b2f8f57
ananyakudtarkar <ananyask.03@gmail.com>
2025-07-24 09:50:45 +0530

    Create simple_pyramid.cpp

---
+++ simple_pyramid.cpp
@@ -0,0 +1,24 @@
+// simple pyramid
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,k;
+    int n=5;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*
+OUTPUT:
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
